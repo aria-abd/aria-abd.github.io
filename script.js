7523ce90f5d7b4a4205d1ef76b2a386eeafde6dd
Aria <119069577+aria-abd@users.noreply.github.com>
2023-11-15 04:17:26 +0330

    as2

--- script.js
+++ script.js
@@ -1,94 +1,28 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'correct Number';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 85;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-let score = 20;
-let highscore = 0;
-
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
-document.querySelector('.score').textContent = score;
-document.querySelector('.highscore').textContent = 0;
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-  //  when there is no input
-  if (!guess) {
-    // document.querySelector('.message').textContent = 'no number';
-    displayMessage('no number');
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
   }
-  // when is corect
-  else if (guess === secretNumber) {
-    debugger;
-    // document.querySelector('.message').textContent = 'correct number';
-    displayMessage('Afarin jojo ,kheyli doset daram');
-    document.querySelector('body').classList.add('js-background');
-    document.querySelector('.number').classList.add('js-number');
-    document.querySelector('.number').textContent = secretNumber;
-
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      // document.querySelector('.message').textContent =
-      //   guess > secretNumber ? 'too high' : 'too low';
-      displayMessage(guess > secretNumber ? 'too high' : 'too low');
-    } else {
-      // document.querySelector('.message').textContent = 'you lost the game';
-      displayMessage('you lost the game');
-      document.querySelector('.score').textContent = 0;
-    }
-    score--;
-    document.querySelector('.score').textContent = score;
-  }
-  // when is too high
-  // else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'too high';
-  //   } else {
-  //     document.querySelector('.message').textContent = 'you lost the game';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  //   score--;
-  //   document.querySelector('.score').textContent = score;
-  // }
-  // when is too low
-  // else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'too low';
-  //   } else {
-  //     document.querySelector('.message').textContent = 'you lost the game';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  //   score--;
-  //   document.querySelector('.score').textContent = score;
-  // }
-});
-
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  // document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage('Start guessing...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('body').classList.remove('js-background');
-  document.querySelector('.number').classList.remove('js-number');
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-  secretNumber;
 });
